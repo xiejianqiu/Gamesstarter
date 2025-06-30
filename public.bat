206b3a210c13b97d9204e16959da7d0fa2d4e4cc
445939112@qq.com <Xjq**gb5059>
2025-06-30 19:49:58 +0800

    [ADD]添加自动发布命令

---
+++ public.bat
@@ -0,0 +1,103 @@
+SET CUR_PATH=%CD%
+SET NEWEXE=%CUR_PATH%\publish\Gamesstarter.exe
+SET MACRO=FRXX_KU25
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_37TANG
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_43U
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_1912YX
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_ZIXIA
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_52GG
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_1171WAN
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_YXA9
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_XINGDIE
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_YILING
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_45YX
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_DY1
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_DY2
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_8090
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_335WAN
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_JIUHOU
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
+
+SET MACRO=FRXX_SHUNGWANG
+SET NEWNAME=%MACRO%.exe
+SET EXE_PUBLICH=dotnet publish -c Release -r win-x64 -o ./publish /p:PublishSingleFile=true /p:DefineConstants=%MACRO%
+CALL %EXE_PUBLICH%
+RENAME %NEWEXE% %NEWNAME%
